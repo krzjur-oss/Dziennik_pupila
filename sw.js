1752c23bcf03d2a2239a96783bf5eca308c5e4a4
krzjur-oss <krzjur@gmail.com>
2026-06-18 08:50:46 +0000

    Deploying to gh-pages from @ krzjur-oss/Dziennik_pupila@a4ef9a570e1e145189495d83000c7da486908daf 🚀

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 const CACHE_NAME = 'dziennik-pupila-v1';
 const ASSETS_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/app_icon.png'
+  './',
+  'index.html',
+  'manifest.json',
+  'app_icon.png'
 ];
 
 // Installation: Cache pre-defined shell assets
